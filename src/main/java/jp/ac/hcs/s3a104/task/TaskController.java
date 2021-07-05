1f73237f999d6e094974d1963622a62c6cd60bad
kou06 <kou06@192.168.0.26>
2021-07-05 11:18:47 +0900

    CSV出力

--- src/main/java/jp/ac/hcs/s3a104/task/TaskController.java
+++ src/main/java/jp/ac/hcs/s3a104/task/TaskController.java
@@ -1,14 +1,19 @@
 package jp.ac.hcs.s3a104.task;
 
+import java.io.IOException;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jp.ac.hcs.s3a104.WebConfig;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -43,13 +48,44 @@ public class TaskController {
 			@RequestParam("limitday") String limitday,
 			Principal principal,Model model) {
 
+		TaskEntity taskEntity = taskService.insert(principal.getName(),comment,limitday);
 		log.info("[" + principal.getName() + "]タスク追加：" + "コメント：" + comment + "期限日：" + limitday);
-		boolean isSuccess = taskService.insert(principal.getName(),comment,limitday);
-		if (isSuccess) {
-			log.info("成功");
-		}else {
-			log.info("失敗");
-		}
 		return getTaskList(principal, model);
+	}
+
+	/**
+	 * 自分の全てのタスク情報をCSVファイルとしてダウンロードさせる.
+	 * @param principal ログイン情報
+	 * @param model
+	 * @return タスク情報のCSVファイル
+	 */
+	@PostMapping("/task/csv")
+	public ResponseEntity<byte[]> getTaskCsv(Principal principal, Model model) {
+
+
+		final String OUTPUT_FULLPATH = WebConfig.OUTPUT_PATH + WebConfig.FILENAME_TASK_CSV;
+
+		log.info("[" + principal.getName() + "]CSVファイル作成:" + OUTPUT_FULLPATH);
+
+		// CSVファイルをサーバ上に作成
+		taskService.taskListCsvOut(principal.getName());
+
+		// CSVファイルをサーバから読み込み
+		byte[] bytes = null;
+		try {
+			bytes = taskService.getFile(OUTPUT_FULLPATH);
+			log.info("[" + principal.getName() + "]CSVファイル読み込み成功:" + OUTPUT_FULLPATH);
+		} catch (IOException e) {
+			log.warn("[" + principal.getName() + "]CSVファイル読み込み失敗:" + OUTPUT_FULLPATH);
+			e.printStackTrace();
+		}
+
+		// CSVファイルのダウンロード用ヘッダー情報設定
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "text/csv; charset=UTF-8");
+		header.setContentDispositionFormData("filename", WebConfig.FILENAME_TASK_CSV);
+
+		// CSVファイルを端末へ送信
+		return new ResponseEntity<byte[]>(bytes, header, HttpStatus.OK);
 	}
 }
--- src/main/java/jp/ac/hcs/s3a104/task/TaskService.java
+++ src/main/java/jp/ac/hcs/s3a104/task/TaskService.java
@@ -1,5 +1,14 @@
 package jp.ac.hcs.s3a104.task;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
@@ -35,8 +44,48 @@ public class TaskService {
 	 * タスクを追加する
 	 * @param
 	 */
-	public boolean insert(String userId, String comment, String limitday) {
-		return false;
+	public TaskEntity insert(String userId, String comment, String limitday) {
+		TaskEntity taskEntity = new TaskEntity();
+		TaskData taskData = new TaskData();
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+		Date due;
+		try {
+			if(comment != ""&& comment.length() < 51 && limitday != "") {
+				due = dateFormat.parse(limitday);
+				taskData.setUser_id(userId);
+				taskData.setComment(comment);
+				taskData.setLimitday(due);
+			}
+		}catch(ParseException e) {
+			e.printStackTrace();
+		}catch(NullPointerException e) {
+			e.printStackTrace();
+		}
+
+		int rowNumber = taskRepository.insertOne(taskData);
+		return taskEntity;
 	}
 
+	/**
+	 * タスク情報をCSVファイルとしてサーバに保存する.
+	 * @param user_id ユーザID
+	 * @throws DataAccessException
+	 */
+	public void taskListCsvOut(String user_id) throws DataAccessException {
+		taskRepository.tasklistCsvOut(user_id);
+	}
+
+	/**
+	 * サーバーに保存されているファイルを取得して、byte配列に変換する.
+	 * @param fileName ファイル名
+	 * @return ファイルのbyte配列
+	 * @throws IOException ファイル取得エラー
+	 */
+	public byte[] getFile(String fileName) throws IOException {
+		FileSystem fs = FileSystems.getDefault();
+		Path p = fs.getPath(fileName);
+		byte[] bytes = Files.readAllBytes(p);
+		return bytes;
+	}
 }
